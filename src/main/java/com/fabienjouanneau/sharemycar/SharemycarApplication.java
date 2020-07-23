79c0c01d46f336acf8ed4c2692c950006e603513
Fabien <jouanneau.fab@gmail.com>
2020-07-23 16:01:48 +0200

    initialize project

---
+++ src/main/java/com/fabienjouanneau/sharemycar/SharemycarApplication.java
@@ -0,0 +1,13 @@
+package com.fabienjouanneau.sharemycar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharemycarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharemycarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabienjouanneau/sharemycar/SharemycarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabienjouanneau.sharemycar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharemycarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
